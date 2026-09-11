89b6aea8811b22873caaf2754488517440ea50d0
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-11 20:49:31 +0530

    build in method in the finsing the largst num

--- list_string/largest_in_list.py
+++ list_string/largest_in_list.py
@@ -3,3 +3,12 @@ lst = [3,5,7,2,7,1,9,4]
 # Finding the largest number in the list using built-in function
 lagestNum = max(lst)
 print("The largest number in the list is: ", lagestNum)
+
+
+# Finding the largest number in the list using for loop
+
+largeNum = lst[0]
+for num in lst:
+    if num > largeNum:
+        largeNum = num
+print("The largest number in the list is: ", largeNum)
