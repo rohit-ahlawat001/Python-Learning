483bfc61b15b3c97f2526e22e9ac782aa43b6e57
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-11 20:47:38 +0530

    find the largest number and did minor changes into the calc

--- basics/calculator.py
+++ basics/calculator.py
@@ -2,6 +2,9 @@ firstNum = int(input("Enter first number: "))
 secondNum = int(input("Enter second number: "))
 selectOperation = input("Select operation (+, -, *, /): ")
 
+if (firstNum == 0 or secondNum == 0) and (selectOperation == "/" or selectOperation == "*"):
+    print("Please enter a number greater than 0")
+
 if selectOperation == "+":
     result = firstNum + secondNum
     print("The result is: ", result)
---
+++ list_string/largest_in_list.py
@@ -0,0 +1,5 @@
+lst = [3,5,7,2,7,1,9,4]
+
+# Finding the largest number in the list using built-in function
+lagestNum = max(lst)
+print("The largest number in the list is: ", lagestNum)
