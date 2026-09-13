56bbc0c6c3b0445862439b68fc83244795a7e723
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-13 09:09:19 +0530

    created the check even odd code

---
+++ basics/even_odd.py
@@ -0,0 +1,14 @@
+# Check if a number is even or odd using the static method
+checkNum = 5
+if checkNum % 2 == 0:
+    print(checkNum, "is an even number")    
+else:
+    print(checkNum, "is an odd number")
+
+
+#check if a number is even or odd using the inut method
+getNum = int(input("Enter a number to check if it is even or odd: "))
+if getNum % 2 == 0:
+    print(getNum, "is an even number")
+else:
+    print(getNum, "is an odd number")
