da16da08e753b56195f612fad77dc20880bbb826
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-13 09:22:43 +0530

    finding postive negative using the input value

--- basics/positive-negative.py
+++ basics/positive-negative.py
@@ -6,3 +6,11 @@ if num > 0:
 else:
     print("The number is Negative")
 
+#fiding positive and negative numbers from the input method
+getNum = int(input("Enter a number to check if it is positive or negative: "))
+if getNum == 0:
+    print("Please enter a number greater than 0 or less than 0")
+elif getNum > 0:
+    print(getNum, "is a Positive number")
+else:
+    print(getNum, "is a Negative number")
