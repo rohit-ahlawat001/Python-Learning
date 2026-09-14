f690cbc0cc9b23799414d44ace2b61bf05ddd665
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-14 20:34:14 +0530

    program in breaf

--- list_string/second_largest.py
+++ list_string/second_largest.py
@@ -10,3 +10,5 @@ for num in numbers:
     elif num > secondLarge and num != largeNum:
         secondLarge = num
 print("The second largest number is:", secondLarge)
+
+# to get the second largest number firstly I get the largest number in the list and then I check if the current number is greater than the second largest number and not equal to the largest number, then I update the second largest number. 
