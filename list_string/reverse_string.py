5ae55ae39842f68d3630b38494ba1e9b187f2547
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-14 20:39:11 +0530

    reverse string using the custom code

--- list_string/reverse_string.py
+++ list_string/reverse_string.py
@@ -2,3 +2,9 @@ str = "Rohit"
 reverse_str = str[::-1]
 print("The reversed string is:", reverse_str)
 # reversing the string using slicing method build-in function
+
+newstr = "Ahlawat"
+newreverse_str = ''
+for char in newstr:
+    newreverse_str = char + newreverse_str
+print("The reversed string is:", newreverse_str)
