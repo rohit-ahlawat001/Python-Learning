62332f958395fb99ee0327a9cbf5ae4ae649f216
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-15 07:54:39 +0530

    handle the even odd zero value validation

--- list_string/even_odd_count.py
+++ list_string/even_odd_count.py
@@ -7,7 +7,9 @@ else:
 
 # Checking the number even or the odd with the user input
 inputNum = int(input("Enter a number: "))
-if inputNum % 2 == 0:
+if inputNum == 0:
+    print(f"{inputNum} is neither even nor odd")
+elif inputNum % 2 == 0:
     print(f"{inputNum} is an even number")
 else:
     print(f"{inputNum} is an odd number")
