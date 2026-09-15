8177ba9d74f1533423c3293377f04ef649d22618
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-15 07:52:06 +0530

    even odd number using the inout value

--- list_string/even_odd_count.py
+++ list_string/even_odd_count.py
@@ -4,3 +4,10 @@ if num % 2 == 0:
     print(f"{num} is an even number")
 else:
     print(f"{num} is an odd number")
+
+# Checking the number even or the odd with the user input
+inputNum = int(input("Enter a number: "))
+if inputNum % 2 == 0:
+    print(f"{inputNum} is an even number")
+else:
+    print(f"{inputNum} is an odd number")
