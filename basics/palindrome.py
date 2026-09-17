63cc8bcb2cc987d5002775159241d453f0c68aea
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-17 07:52:57 +0530

    added the else condition for string not palindrome

--- basics/palindrome.py
+++ basics/palindrome.py
@@ -2,3 +2,5 @@ string = str(input("Enter a string: "))
 new_string = string[::-1]
 if string == new_string:
     print(f"{string} is a palindrome.")
+else:
+    print(f"{string} is not a palindrome.")
