faca28c74ddf411ba4d13ef69623e2d0db0546a3
Rohit Ahlawat <rohitahlawt160@gmail.com>
2026-09-25 22:38:28 +0530

    solving student grade proble

--- basics/grade_calculator.py
+++ basics/grade_calculator.py
@@ -3,3 +3,9 @@
 marks = int(input(" Enter the student marks"))
 print(marks)
 
+if marks >=70 and marks<=100:
+    print("Grade A")
+elif marks >= 60 and marks <= 69:
+    print("Grade B")
+elif marks >= 35 and marks <= 59:
+    print
